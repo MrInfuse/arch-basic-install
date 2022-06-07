effeec6c7a8e52479af337e01b4999367c75a730
MrInfuse <tjohndereck@gmail.com>
2022-06-07 13:51:41 +0800

    init

---
+++ base.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Asia/Manila /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "timbreza" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 timbreza.localdomain timbreza" >> /etc/hosts
+echo root:password | chpasswd
+
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+# You can remove the tlp package if you are installing on a desktop or vm
+
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers xdg-user-dirs xdg-utils bluez bluez-utils pipewire pipewire-pulse bash-completion openssh rsync acpi acpi_call tlp acpid os-prober ntfs-3g terminus-font dosfstools exfat-utils
+
+pacman -S --noconfirm xf86-video-intel xf86-video-amdgpu
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=LOGOS #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable sshd
+systemctl enable tlp
+systemctl enable fstrim.timer
+systemctl enable acpid
+
+useradd -m j
+echo j:password | chpasswd
+
+echo "j ALL=(ALL) ALL" >> /etc/sudoers.d/j
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
---
+++ dwm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Options
+aur_helper=true
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+# sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+# sudo firewall-cmd --add-port=1025-65535/udp --permanent
+# sudo firewall-cmd --reload
+
+if [[ $aur_helper = true ]]; then
+    cd /tmp
+    git clone https://aur.archlinux.org/paru.git
+    cd paru/;makepkg -si --noconfirm;cd
+fi
+
+# Install packages
+sudo pacman -S xorg firefox polkit-gnome xwallpaper lxappearance ranger ueberzug bc picom arandr	libnotify dunst sxiv ffmpeg ffmpegthumbnailer gnome-keyring neovim mpd mpc mpv man-db ncmpcpp noto-fonts-emoji pamixer maim unclutter unzip lynx xcape xclip xdotool yt-dlp zathura zathura-pdf-mupdf poppler mediainfo atool fzf bat moreutils
+
+paru -S --noconfirm brave-bin
+
+# Install fonts
+sudo pacman -S --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+
+# Pull Git repositories and install
+cd /tmp
+repos=( "dmenu" "dwm" "st" )
+for repo in ${repos[@]}
+do
+    git clone git://git.suckless.org/$repo
+    cd $repo;make;sudo make install;cd ..
+done
+
+printf "\e[1;32mDone! you can now reboot.\e[0m\n"
