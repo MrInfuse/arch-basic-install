4d8471f4f9c01f7f5ece7947fa7b736b166f5f7d
MrInfuse <39250321+MrInfuse@users.noreply.github.com>
2022-06-11 18:21:56 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -14,7 +14,7 @@ echo root:password | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers xdg-user-dirs xdg-utils bluez bluez-utils pipewire pipewire-pulse bash-completion openssh rsync acpi acpi_call tlp acpid os-prober ntfs-3g terminus-font dosfstools exfat-utils
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers xdg-user-dirs xdg-utils bluez bluez-utils pipewire pipewire-pulse bash-completion zsh openssh rsync acpi acpi_call tlp acpid os-prober ntfs-3g terminus-font dosfstools exfat-utils
 
 pacman -S --noconfirm xf86-video-intel xf86-video-amdgpu
 
